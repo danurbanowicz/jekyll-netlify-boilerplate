6191e40cabc89924b4a5857c74c7fedb3b991222
Dan Urbanowicz <dan.urbanowicz@gmail.com>
2018-07-20 09:15:20 +0200

    Remove unused functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,4 @@ window.onload=function(){
   // Set the page state on load
   document.documentElement.setAttribute("data-state","loaded");
 
-  // If the browser doesn not support CSS Grid, show a warning message
-  var a=CSS.supports("display:grid");
-
-  if(!a){
-    var b=document.createElement("div");
-    b.innerHTML='It looks like your browser doesn\'t support the latest features used on this website. Please consider <a href="https://www.google.com/chrome/">upgrading your browser</a> for the best experience.';
-    document.body.appendChild(b)
-  }
-
 };
